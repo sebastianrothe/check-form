c4730d1d89bbb07e9ce179b7367af5099af71f55
Sebastian Rothe <rothe@infai.org>
2019-10-06 10:12:24 +0200

    fix retry with filter

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,29 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+function waitForResourceToLoad(fileName, type) {
+  const resourceCheckInterval = 40
+
+  return new Cypress.Promise(resolve => {
+    const checkIfResourceHasBeenLoaded = () => {
+      const resource = cy
+        .state('window')
+        .performance.getEntriesByType('resource')
+        .filter(entry => !type || entry.initiatorType === type)
+        .find(entry => entry.name.includes(fileName))
+
+      if (resource) {
+        resolve()
+
+        return
+      }
+
+      setTimeout(checkIfResourceHasBeenLoaded, resourceCheckInterval)
+    }
+
+    checkIfResourceHasBeenLoaded()
+  })
+}
+
+Cypress.Commands.add('waitForResourceToLoad', waitForResourceToLoad)
