fa2a6e5c7e7fa7eafb7d697efe8e08d3b8bcfcf4
Sebastian Rothe <rothe@infai.org>
2019-10-06 10:56:42 +0200

    replace wait with promise then

--- cypress/integration/leipzig/datepicker.spec.js
+++ cypress/integration/leipzig/datepicker.spec.js
@@ -23,33 +23,36 @@ context('Anmeldung Leipzig', () => {
       .screenshot()
   })
 
-  it('has a form, with values', async () => {
-    const [name, datepicker, people, coupon, mail, phone] = await cy.get(
-      '#contact-form-18 form div input'
-    )
+  it('has a form, with values', () => {
+    return cy
+      .get('#contact-form-18 form div input')
+      .spread((name, datepicker, people, coupon, mail, phone) => {
+        // fill form
+        cy.wrap(name)
+          .type('Testuser')
+          .should('have.value', 'Testuser')
+        cy.wrap(datepicker)
+          .type('31.12.2022', { force: true })
+          .should('have.value', '31.12.2022')
+        cy.wrap(datepicker).type('{esc}', { force: true })
+        // cy.wrap(datepicker).click({ force: true })
+        // cy.wait(1000)
+        cy.wrap(people)
+          .type('1')
+          .should('have.value', '1')
+        cy.wrap(mail)
+          .type('kontakt@gruseltour-leipzig.de')
+          .should('have.value', 'kontakt@gruseltour-leipzig.de')
+        cy.wrap(phone)
+          .type('+49016096776494')
+          .should('have.value', '+49016096776494')
 
-    // fill form
-    cy.wrap(name)
-      .type('Testuser')
-      .should('have.value', 'Testuser')
-    cy.wrap(datepicker)
-      .type('31.12.2022', { force: true })
-      .should('have.value', '31.12.2022')
-      .type('{esc}', { force: true })
-    cy.wait(1000)
-    cy.wrap(people)
-      .type('1')
-      .should('have.value', '1')
-    cy.wrap(mail)
-      .type('kontakt@gruseltour-leipzig.de')
-      .should('have.value', 'kontakt@gruseltour-leipzig.de')
-    cy.wrap(phone)
-      .type('+49016096776494')
-      .should('have.value', '+49016096776494')
+        // cy.screenshot()
 
-    cy.screenshot()
-
-    // submit form
-    cy.get('#contact-form-18 form .contact-submit button[type=submit]').click()
+        // submit form
+        cy.get(
+          '#contact-form-18 form .contact-submit button[type=submit]'
+        ).click()
+      })
   })
 })
