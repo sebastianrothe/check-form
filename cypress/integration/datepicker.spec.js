2541fa8a2eca4f98e1494758e4571ec06e95259d
Sebastian Rothe <rothe@infai.org>
2019-10-06 10:13:02 +0200

    rename file get form working

---
+++ cypress/integration/datepicker.spec.js
@@ -0,0 +1,55 @@
+context('Anmeldung Leipzig', () => {
+  beforeEach(() => {
+    cy.visit('https://gruseltour-leipzig.de/anmeldung/')
+    cy.waitForResourceToLoad('jquery-3.4.1.min.js')
+  })
+
+  it('has a datepicker, which is readonly', () => {
+    const datepickerSelector =
+      '#contact-form-18 form div input[type=text].hasDatepicker'
+
+    cy.get(datepickerSelector).should('have.attr', 'readonly')
+  })
+
+  it('has a datepicker, which is opens, when clicked', () => {
+    const datepickerSelector =
+      '#contact-form-18 form div input[type=text].hasDatepicker'
+
+    cy.get(datepickerSelector)
+      .should('have.class', 'hasDatepicker')
+      .should('have.class', 'ui-datepicker-readonly')
+      .click({ force: true })
+      .scrollIntoView()
+      .screenshot()
+  })
+
+  it.only('has a form, with values', async () => {
+    const [name, datepicker, people, coupon, mail, phone] = await cy.get(
+      '#contact-form-18 form div input'
+    )
+
+    // fill form
+    cy.wrap(name)
+      .type('Testuser')
+      .should('have.value', 'Testuser')
+    cy.wrap(datepicker)
+      .type('31.12.2022', { force: true })
+      .should('have.value', '31.12.2022')
+      .type('{esc}', { force: true })
+    cy.wait(1000)
+    cy.wrap(people)
+      .type('1')
+      .should('have.value', '1')
+    cy.wrap(mail)
+      .type('kontakt@gruseltour-leipzig.de')
+      .should('have.value', 'kontakt@gruseltour-leipzig.de')
+    cy.wrap(phone)
+      .type('+49016096776494')
+      .should('have.value', '+49016096776494')
+
+    cy.screenshot()
+
+    // submit form
+    cy.get('#contact-form-18 form .contact-submit button[type=submit]').click()
+  })
+})
