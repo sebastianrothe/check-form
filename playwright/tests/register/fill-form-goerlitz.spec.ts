24755443616f38503fb54c134d2fbb4f1648643f
Sebastian Rothe <rothe@infai.org>
2022-12-16 13:49:41 +0100

    chore: move test into subfolder rename fille

--- playwright/tests/register/fill-form-goerlitz.spec.ts
+++ playwright/tests/register/fill-form-goerlitz.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://gruseltour-goerlitz.de/');
-  
+
   await page.getByLabel('Wann möchtest du die Tour buchen? Die Touren finden Freitag, Samstag und auch an Feiertagen statt. *').click();
   await page.getByRole('link', { name: '20' }).click();
   await page.getByLabel('Wie heißt du (Vorname)? *').click();
@@ -14,8 +14,9 @@ test('test', async ({ page }) => {
   await page.getByLabel('Und deine Handynummer, damit wir dich im Notfall erreichen können: *').click();
   await page.getByLabel('Und deine Handynummer, damit wir dich im Notfall erreichen können: *').fill('123');
 
-  await page.getByRole('button', { name: 'Anmelden' }).click();
+  await page.getByRole('button', { name: 'Anmelden' }).click()
+  await page.waitForNavigation({url:"https://gruseltour-goerlitz.de/anmeldung-erfolgreich/"});
 
-  await expect(page).toHaveURL(/.*anmeldung-erfolgreich/);
+  await expect(page).toHaveURL("https://gruseltour-goerlitz.de/anmeldung-erfolgreich/");
   await page.getByRole('heading', { name: 'Anmeldung erfolgreich' }).click();
 });
