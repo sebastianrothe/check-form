874dd3316d074d109dde8095e5514d210200cd89
Sebastian Rothe <rothe@infai.org>
2022-12-21 11:37:51 +0100

    fix: make date more dynamic

--- tests/register/fill-form-goerlitz.spec.ts
+++ tests/register/fill-form-goerlitz.spec.ts
@@ -4,13 +4,23 @@ test('test', async ({ page }) => {
   await page.goto('https://gruseltour-goerlitz.de/');
 
   await page.getByLabel('Wann möchtest du die Tour buchen? Die Touren finden Freitag, Samstag und auch an Feiertagen statt. *').click();
-  await page.getByRole('link', { name: '20' }).click();
+  await page.locator('a').filter({ hasText: 'Vor&#x3E;' }).click();
+  await page.locator('a').filter({ hasText: 'Vor&#x3E;' }).click();
+  await page.locator('a').filter({ hasText: 'Vor&#x3E;' }).click();
+  await page.locator('a').filter({ hasText: 'Vor&#x3E;' }).click();
+  await page.locator('a').filter({ hasText: 'Vor&#x3E;' }).click();
+  await page.locator('a').filter({ hasText: 'Vor&#x3E;' }).click();
+  await page.getByRole('link', { name: '15' }).click();
+
   await page.getByLabel('Wie heißt du (Vorname)? *').click();
   await page.getByLabel('Wie heißt du (Vorname)? *').fill('Playwright');
+
   await page.getByLabel('Wie viele Personen wollen an der Tour teilnehmen? *').click();
   await page.getByLabel('Wie viele Personen wollen an der Tour teilnehmen? *').fill('2');
+
   await page.getByLabel('Für die Bestätigung benötigen wir deine E-Mail-Adresse: *').click();
   await page.getByLabel('Für die Bestätigung benötigen wir deine E-Mail-Adresse: *').fill('kontakt@gruseltour-goerlitz.de');
+
   await page.getByLabel('Und deine Handynummer, damit wir dich im Notfall erreichen können: *').click();
   await page.getByLabel('Und deine Handynummer, damit wir dich im Notfall erreichen können: *').fill('123');
 
